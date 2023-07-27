fa5cc1c380a49ee241436f6d708b31fd15f48c5b
Jayce <jturambe@gmail.com>
2023-07-26 22:38:46 -0500

    Added a method to update our stock when an Item is taken out

--- IncomingTransaction.java
+++ IncomingTransaction.java
@@ -2,21 +2,32 @@ package store_info;
 import java.util.*;
 
 /**
- * 
+ * This class represents an incoming transaction in the inventory
+ * management system.An incoming transaction is used when adding 
+ * new items to the inventory.It extends the base Transaction class, 
+ * inheriting its attributes and methods.
  */
 public class IncomingTransaction extends Transaction {
 
     /**
-     * Default constructor
+     * Default constructor to initialize an incoming transaction.
      */
     public IncomingTransaction() {
     }
 
     /**
-     * 
+     * This method is overridden from the base Transaction class to update 
+     * the product stock.When an incoming transaction occurs, it adds new 
+     * items to the inventory.The implementation of this method will handle 
+     * updating the inventory stock accordingly.
      */
+    @Override
     public void updateProductStock() {
-        // TODO implement here
+        for (Map.Entry<Product, Integer> entry : getProductList().entrySet()) {
+            Product product = entry.getKey();
+            int numberOfItems = entry.getValue();
+            product.setNumberOfItems(product.getNumberOfItems() + numberOfItems);
+        }
     }
 
 }
