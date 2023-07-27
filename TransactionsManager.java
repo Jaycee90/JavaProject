7cc13fe164fddbcf87790be01a4d14873adea8c3
Jayce <jturambe@gmail.com>
2023-07-26 22:35:17 -0500

    Added a method to add transaction and update the stock

--- TransactionsManager.java
+++ TransactionsManager.java
@@ -2,26 +2,40 @@ package store_info;
 import java.util.*;
 
 /**
- * 
+ * This class manages transactions in the inventory 
+ * management system. It allows adding new transactions 
+ * and appending transaction information to a file. 
  */
 public class TransactionsManager {
+    //
+    private List<Transaction> transactions;
 
     /**
-     * Default constructor
+     * Default constructor to initialize the TransactionsManager.
      */
     public TransactionsManager() {
+        //Initialize the list 
+        transactions = new ArrayList<>();
     }
 
     /**
-     * @param Transaction tr
+     * Adds a new transaction to the system.
+     * @param transaction The Transaction object representing 
+     * the new transaction to be added.
      */
     public void addTransaction(Transaction transaction) {
-        // TODO implement here
+        //Depending on the transaction type (Incoming or Outgoing), 
+        //you need to update the stock accordingly
+        transactions.add(transaction);
+        transaction.updateProductStock();
     }
 
     /**
-     * @param String fileName 
-     * @param String transaction
+     * Appends transaction information to a file.
+     * @param fileName The name of the file to which 
+     * the transaction information will be appended.
+     * @param transaction The String representation of 
+     * the transaction information to be appended.
      */
     public void appendToFile(String fileName, String transaction) {
         // TODO implement here
