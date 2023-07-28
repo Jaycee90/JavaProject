dbfb66fe2d3a5d287bf898728fffbefe3d3be459
Jayce <jturambe@gmail.com>
2023-07-28 18:05:43 -0500

    I emplimented a method thet prints report

--- Report.java
+++ Report.java
@@ -2,8 +2,18 @@ package store_info;
 import java.util.*;
 
 /**
- * 
+ * The Report interface represents a contract for generating store reports.
+ * Classes that implement this interface are responsible for generating 
+ * specific types of reports. Each implementing class should provide 
+ * an implementation for the report generation logic.
  */
 public interface Report {
+    /*
+     * Generates the specific report based on store data. 
+     * The implementing class should define the report generation 
+     * logic here. The specific details and format of the report 
+     * should be described in the implementation.
+     */
+    void printReport();
 
 }
