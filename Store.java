6aaba4bbd681497645605c7cf3f6825b02ca6fb9
Jayce <jturambe@gmail.com>
2023-07-28 18:14:33 -0500

    Initialisation of constructor and implimented getters to get information of stores

--- Store.java
+++ Store.java
@@ -6,25 +6,33 @@ import java.util.*;
  */
 public class Store {
 
+    private int ID;
+    private String name;
+    private String address;
+
     /**
      * Default constructor
      */
     public Store() {
     }
 
-    /**
-     * 
-     */
-    private int ID;
+    //Constructor with arguments
+    public Store (int ID, String name, String address){
+        this.ID = ID;
+        this.name = name;
+        this.address = address;
+    }
 
-    /**
-     * 
-     */
-    private String name;
+    public String getName(){
+        return name;
+    }
 
-    /**
-     * 
-     */
-    private String address;
+    public int getID(){
+        return ID;
+    }
+
+    public String getAddress(){
+        return address;
+    }
 
 }
