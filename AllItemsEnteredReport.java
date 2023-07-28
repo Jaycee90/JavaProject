23630ec78c24df47ce5ae52677e998466418b002
Jayce <jturambe@gmail.com>
2023-07-28 17:57:30 -0500

    I added getters to get the names and ID of new items in the stock. I declared the fields but before implimenting getters, the files has copmilation errors

--- AllItemsEnteredReport.java
+++ AllItemsEnteredReport.java
@@ -1,23 +1,46 @@
 package store_info;
 
-import java.util.*;
+//import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 
  */
 public class AllItemsEnteredReport implements Report {
 
+    private TransactionsManager transactionsManager;
+
     /**
-     * Default constructor
+     * Constructor to initialize the AllItemsEnteredReport with the TransactionsManager.
+     *
      */
-    public AllItemsEnteredReport() {
+    public AllItemsEnteredReport(TransactionsManager transactionsManager) {
+        this.transactionsManager = transactionsManager;
     }
 
     /**
-     * 
+     * Print the report of all items entered in the inventory.
      */
+    @Override
     public void printReport() {
-        // TODO implement here
+        List<Transaction> transactions = transactionsManager.getTransactions();
+        System.out.println("All Items Entered Report:");
+
+        for (Transaction transaction : transactions){
+            if (transaction instanceof IncomingTransaction){
+                System.out.println("Transasction ID: " + transaction.getID());
+                System.out.println("Date: " + transaction.getDate());
+                System.out.println("Products: ");
+                for (Map.Entry<Product, Integer> entry : transaction.getProductList().entrySet()){
+                    Product product = entry.getKey();
+                    int numberOfItems = entry.getValue();
+                    System.out.println(product.getName() + "(ID: " + product.getID() + ") -Quantity: " + numberOfItems);
+
+                }
+                System.out.println("-----------------------------------");
+            }
+        }
     }
 
     /**
@@ -25,6 +48,10 @@ public class AllItemsEnteredReport implements Report {
      */
     private void collectItemsEntered() {
         // TODO implement here
+    }
+
+    public List<Transaction> getTransactions(){
+        return transactionsManager.getTransactions();
     }
 
 }
