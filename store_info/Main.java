2e0d247065ec0cd91bd8295c473a73a17d39a7a0
Jayce <jturambe@gmail.com>
2023-07-28 23:05:25 -0500

    Added a store inventory menu

--- store_info/Main.java
+++ store_info/Main.java
@@ -6,6 +6,7 @@ import java.util.*;
  * information system. It contains references to various 
  * managers responsible for managing different aspects of the store.
  */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,36 +15,71 @@ public class Main {
         ReportsManager reportsManager = new ReportsManager();
         TransactionsManager transactionsManager = new TransactionsManager();
 
-        // Create some products and stores
-        Product product1 = new Product(1, "Product 1");
-        Product product2 = new Product(2, "Product 2");
-        Store store1 = new Store(1, "Store 1", "Address 1");
-        Store store2 = new Store(2, "Store 2", "Address 2");
+        // Create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
 
-        // Add products and stores to the inventory
-        stockManager.addProduct(product1);
-        stockManager.addProduct(product2);
-        stockManager.addStore(store1);
-        stockManager.addStore(store2);
+        while (true) {
+            System.out.println("Inventory Management System Menu");
+            System.out.println("p: Add Product");
+            System.out.println("s: Add Store");
+            System.out.println("i: Perform Incoming Transaction");
+            System.out.println("o: Perform Outgoing Transaction");
+            System.out.println("r: Generate Reports");
+            System.out.println("x: Exit Program");
 
-        // Perform incoming transaction
-        IncomingTransaction incomingTransaction = new IncomingTransaction(1, new Date());
-        incomingTransaction.addProduct(product1, 100);
-        incomingTransaction.addProduct(product2, 50);
-        transactionsManager.addTransaction(incomingTransaction);
+            System.out.print("Input an action: ");
+            String input = scanner.nextLine();
 
-        // Perform outgoing transaction
-        OutgoingTransaction outgoingTransaction = new OutgoingTransaction(2, new Date());
-        outgoingTransaction.addProduct(product1, 30);
-        transactionsManager.addTransaction(outgoingTransaction);
+            switch (input.toLowerCase()) {
+                case "p":
+                    System.out.print("Enter product name: ");
+                    String productName = scanner.nextLine();
+                    System.out.print("Enter product amount: ");
+                    int productAmount = Integer.parseInt(scanner.nextLine());
+                    Product product = new Product(stockManager.getNextProductID(), productName);
+                    product.setNumberOfItems(productAmount);
+                    stockManager.addProduct(product);
+                    break;
 
-        // Generate reports
-        Report availableItemsReport = new AvailableItemsReport(stockManager);
-        Report allItemsEnteredReport = new AllItemsEnteredReport(transactionsManager);
-        Report productByStoreReport = new ProductByStoreReport(stockManager, store1);
+                case "s":
+                    System.out.print("Enter store name: ");
+                    String storeName = scanner.nextLine();
+                    System.out.print("Enter store address: ");
+                    String storeAddress = scanner.nextLine();
+                    Store store = new Store(stockManager.getNextStoreID(), storeName, storeAddress);
+                    stockManager.addStore(store);
+                    break;
 
-        reportsManager.generateReport(availableItemsReport);
-        reportsManager.generateReport(allItemsEnteredReport);
-        reportsManager.generateReport(productByStoreReport);
+                case "i":
+                    // Perform incoming transaction
+                    // Implementation code here...
+                    break;
+
+                case "o":
+                    // Perform outgoing transaction
+                    // Implementation code here...
+                    break;
+
+                case "r":
+                    System.out.println("Generating reports...");
+                    // Generate reports
+                    Report availableItemsReport = new AvailableItemsReport(stockManager);
+                    Report allItemsEnteredReport = new AllItemsEnteredReport(transactionsManager);
+
+                    reportsManager.generateReport(availableItemsReport);
+                    reportsManager.generateReport(allItemsEnteredReport);
+                    // You can add more reports here if needed
+                    break;
+
+                case "x":
+                    System.out.println("Exiting program...");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid input. Please try again.");
+            }
+        }
     }
 }
