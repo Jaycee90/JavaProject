78480205f2cb831716e70f0b2383f78abea52831
Jayce <jturambe@gmail.com>
2023-07-30 00:26:01 -0500

    Available items are writen to AvailableItemsReport.txt

--- store_info/AvailableItemsReport.java
+++ store_info/AvailableItemsReport.java
@@ -1,4 +1,6 @@
 package store_info;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -19,14 +21,31 @@ public class AvailableItemsReport implements Report {
      Generate and print the report of all available items in the inventory. 
      //
      */
-    @Override
-   public void printReport() {
-       System.out.println("Available Items Report:");
-       List<Product> products = stockManager.getProducts();
-       for (Product product : products) {
-           int numberOfItems = product.getNumberOfItems();
-           System.out.println(product.getName() + " (ID: " + product.getID() + ") - Quantity: " + numberOfItems);
-       }
-   }
+//     @Override
+//    public void printReport() {
+//        System.out.println("Available Items Report:");
+//        System.out.println("Products appear in the format: name(id): amount");
+//        List<Product> products = stockManager.getProducts();
+//        for (Product product : products) {
+//            int numberOfItems = product.getNumberOfItems();
+//            System.out.println(product.getName() + " (" + product.getID() + "): " + numberOfItems);
+//            //System.out.println(product.getName() + " (ID: " + product.getID() + ") - Quantity: " + numberOfItems);
+//        }
+//    }
+    @Override    
+    public void printReport() {
+        List<Product> products = stockManager.getProducts();
+        try (FileWriter fileWriter = new FileWriter("AvailableItemsReport.txt")) {
+            fileWriter.write("Available Items Report:\n");
+            fileWriter.write("Products appear in the format: name(id): amount\n");
+            for (Product product : products) {
+                int numberOfItems = product.getNumberOfItems();
+                fileWriter.write(product.getName() + " (" + product.getID() + "): " + numberOfItems + "\n");
+            }
+            System.out.println("Report saved to AvailableItemsReport.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
